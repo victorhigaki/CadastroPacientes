d616ba26695888b0ed67bf08ccd9c2d718675e36
Victor Higaki <victorhigaki@outlook.com>
2025-05-23 01:04:25 -0300

    feat: base-input with material

---
+++ src/CadastroPacientes/src/app/components/base-input/base-input.component.ts
@@ -0,0 +1,45 @@
+import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NgControl } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-base-input',
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatIconModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './base-input.component.html',
+  styleUrl: './base-input.component.scss'
+})
+export class BaseInputComponent implements ControlValueAccessor {
+  label = input('');
+  placeholder = input('');
+  hint = input('');
+  icon = input('');
+  inputValue = '';
+
+  private ngControl = inject(NgControl, { optional: true })
+  protected onTouched?: () => {}
+  protected onChange?: (value: string) => {};
+  protected isDisabled = false;
+
+  constructor() {
+    if (this.ngControl) this.ngControl.valueAccessor = this;
+  }
+
+  writeValue(obj: any): void {
+    this.inputValue = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+}
