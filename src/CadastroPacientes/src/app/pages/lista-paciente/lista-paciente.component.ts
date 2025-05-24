08216e017b3fa259865ff98933a214b1b53ee57d
Victor Higaki <victorhigaki@outlook.com>
2025-05-24 14:06:57 -0300

    feat: Lista Paciente Page

--- src/CadastroPacientes/src/app/pages/lista-paciente/lista-paciente.component.ts
+++ src/CadastroPacientes/src/app/pages/lista-paciente/lista-paciente.component.ts
@@ -1,10 +1,23 @@
-import { AsyncPipe, JsonPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { ColumnMode, NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { Paciente } from '../../models/paciente';
 import { PacientesService } from '../../services/pacientes.service';
+import { BaseButtonComponent } from '../../components/base-button/base-button.component';
+import { RouterLink } from '@angular/router';
+import { UfService } from '../../services/uf.service';
 
 @Component({
   selector: 'app-lista-paciente',
-  imports: [AsyncPipe, JsonPipe],
+  imports: [
+    CommonModule,
+    NgxDatatableModule,
+    MatIconModule,
+    BaseButtonComponent,
+    RouterLink,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './lista-paciente.component.html',
   styleUrl: './lista-paciente.component.scss',
   providers: [PacientesService],
@@ -13,4 +26,14 @@ export class ListaPacienteComponent {
   pacientesService = inject(PacientesService);
 
   getPacientes$ = this.pacientesService.getAll();
+  pacientes: Paciente[] = [];
+
+  // rows = [
+  //   { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+  //   { name: 'Dany', gender: 'Male', company: 'KFC' },
+  //   { name: 'Molly', gender: 'Female', company: 'Burger King' },
+  // ];
+
+  ColumnMode = ColumnMode;
+
 }
